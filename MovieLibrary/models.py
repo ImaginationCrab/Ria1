d50ead2ee23c5e0469386d3024d46f5edf143938
ImaginationCrab <alexgebroward@gmail.com>
2025-02-07 23:01:55 -0500

    movielibrary page and some models added

--- MovieLibrary/models.py
+++ MovieLibrary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    rating = models.FloatField(default=0)
+    def __str__(self):
+        return self
