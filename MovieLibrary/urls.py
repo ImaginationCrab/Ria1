6da8d7d62b33c9dfe49096e1a9e4e1556eed9728
ImaginationCrab <alexgebroward@gmail.com>
2025-02-10 17:06:44 -0500

    more pages

--- MovieLibrary/urls.py
+++ MovieLibrary/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
 
-    path("", views.index, name = "index"),
+    path("", views.landing, name = "landing"),
+    path("registration", views.registration, name = "registration"),
+    path("login", views.login, name = "login"),
+    path("logout", views.logout, name = "logout"),
+    path("home", views.home, name = "home"),
+
     ]
