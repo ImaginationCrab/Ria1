e742f46312d37f024a28ca5c2276002c680bd95a
unknown <alexgebroward@gmail.com>
2025-02-18 21:57:24 -0500

    changepassword

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('registration/', views.registration, name='accounts.registration'),
     path("login/", views.login, name="login"),
     path('orders/', views.orders, name='accounts.orders'),
+    path('change_password',views.change_password, name='accounts.change_password'),
 ]
